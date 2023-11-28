e246f3e08fd518bd5592f29b0dabb3d18bd89f54
kithhhhh <kthlbarrientos@gmail.com>
2023-11-28 09:18:55 +0800

    Certifications Updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -174,3 +174,19 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+//Accordion
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
